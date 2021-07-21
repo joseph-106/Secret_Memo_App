6ddb9ee785b94c6ce5f38168f0219e1875bc9aa7
unknown <sjjoseph106@gmail.com>
2021-07-21 16:40:42 +0900

    Activity 전환

--- app/src/main/java/com/comu/android/secretmemo/MainActivity.kt
+++ app/src/main/java/com/comu/android/secretmemo/MainActivity.kt
@@ -1,31 +1,32 @@
 package com.comu.android.secretmemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val editText1 = findViewById<EditText>(R.id.Email)
-        val editText2 = findViewById<EditText>(R.id.Password)
+        val editText = findViewById<EditText>(R.id.Password)
 
         findViewById<Button>(R.id.button).setOnClickListener {
-            val emailText = editText1.text.toString()
-            val passwordText = editText2.text.toString()
+            val passwordText = editText.text.toString()
+            val intent = Intent(this, MemoActivity::class.java)
 
-            if (emailText == "sjjoseph106@gmail.com" && passwordText == "3242") {
-                Log.d("비밀메모앱", "통과!")
-            } else if (emailText != "sjjoseph106@gmail.com" && passwordText == "3242") {
-                Log.d("비밀메모앱", "이메일이 틀렸어요.")
-            } else if (emailText == "sjjoseph106@gmail.com" && passwordText != "3242") {
+            if (passwordText != "3242") {
                 Log.d("비밀메모앱", "비밀번호가 틀렸어요.")
+                findViewById<TextView>(R.id.textView).text = "비밀번호가 틀렸어요."
+
             } else {
-                Log.d("비밀메모앱", "이메일과 비밀번호가 틀렸어요.")
+                Log.d("비밀메모앱", "통과!")
+                startActivity(intent)
+                this.finish()
             }
         }
 
