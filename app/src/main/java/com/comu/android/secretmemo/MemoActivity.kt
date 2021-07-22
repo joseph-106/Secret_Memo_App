7289486e598363d0fa99cf28d6462cb83a00b0e0
unknown <sjjoseph106@gmail.com>
2021-07-22 11:23:34 +0900

    텍스트 공유하기 기능 추가

--- app/src/main/java/com/comu/android/secretmemo/MemoActivity.kt
+++ app/src/main/java/com/comu/android/secretmemo/MemoActivity.kt
@@ -1,11 +1,28 @@
 package com.comu.android.secretmemo
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MemoActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_memo)
+
+        val shareButton = findViewById<FloatingActionButton>(R.id.floatingActionButton)
+        val editText = findViewById<EditText>(R.id.editText)
+
+        shareButton.setOnClickListener {
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, editText.text.toString())
+                type = "text/plain"
+            }
+
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
     }
 }
